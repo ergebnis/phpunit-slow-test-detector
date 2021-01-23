338fd17da0e358bc1c97738cf44a497fa8a526f5
Andreas Möller <am@localheinz.com>
2021-01-23 11:05:40 +0100

    Enhancement: Add example tests

---
+++ test/Example/ExampleTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2021 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/phpunit-slow-test-collector
+ */
+
+namespace Ergebnis\PHPUnit\SlowTestCollector\Test\Example;
+
+use Ergebnis\PHPUnit\SlowTestCollector\Test\Fixture;
+use Ergebnis\Test\Util;
+use PHPUnit\Framework;
+
+/**
+ * @internal
+ *
+ * @covers \Ergebnis\PHPUnit\SlowTestCollector\Test\Fixture\Sleeper
+ */
+final class ExampleTest extends Framework\TestCase
+{
+    use Util\Helper;
+
+    public function testSleeperDoesNotSleepAtAll(): void
+    {
+        $milliseconds = 0;
+
+        $sleeper = Fixture\Sleeper::fromMilliseconds($milliseconds);
+
+        $sleeper->sleep();
+
+        self::assertSame($milliseconds, $sleeper->milliseconds());
+    }
+
+    public function testSleeperSleepsOneQuarterOfASecond(): void
+    {
+        $milliseconds = 250;
+
+        $sleeper = Fixture\Sleeper::fromMilliseconds($milliseconds);
+
+        $sleeper->sleep();
+
+        self::assertSame($milliseconds, $sleeper->milliseconds());
+    }
+
+    public function testSleeperSleepsHalfASeconds(): void
+    {
+        $milliseconds = 500;
+
+        $sleeper = Fixture\Sleeper::fromMilliseconds($milliseconds);
+
+        $sleeper->sleep();
+
+        self::assertSame($milliseconds, $sleeper->milliseconds());
+    }
+
+    public function testSleeperSleepsThreeQuartersOfASecond(): void
+    {
+        $milliseconds = 750;
+
+        $sleeper = Fixture\Sleeper::fromMilliseconds($milliseconds);
+
+        $sleeper->sleep();
+
+        self::assertSame($milliseconds, $sleeper->milliseconds());
+    }
+
+    public function testSleeperSleepsOneSecond(): void
+    {
+        $milliseconds = 1000;
+
+        $sleeper = Fixture\Sleeper::fromMilliseconds($milliseconds);
+
+        $sleeper->sleep();
+
+        self::assertSame($milliseconds, $sleeper->milliseconds());
+    }
+}
---
+++ test/Fixture/Sleeper.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2021 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/phpunit-slow-test-collector
+ */
+
+namespace Ergebnis\PHPUnit\SlowTestCollector\Test\Fixture;
+
+final class Sleeper
+{
+    private int $milliseconds;
+
+    private function __construct(int $seconds)
+    {
+        $this->milliseconds = $seconds;
+    }
+
+    /**
+     * @throws \InvalidArgumentException
+     */
+    public static function fromMilliseconds(int $milliseconds): self
+    {
+        if (0 > $milliseconds) {
+            throw new \InvalidArgumentException(\sprintf(
+                'Value for milliseconds should be greater than or equal to 0, but %d is not.',
+                $milliseconds
+            ));
+        }
+
+        return new self($milliseconds);
+    }
+
+    public function milliseconds(): int
+    {
+        return $this->milliseconds;
+    }
+
+    public function sleep(): void
+    {
+        \usleep($this->milliseconds * 1000);
+    }
+}
