40fff1c36921722e0a3fb840fd5fc3a9213f67e8
Andreas Möller <am@localheinz.com>
2021-01-25 09:40:03 +0100

    Enhancement: Implement TestSuiteFinishedSubscriber

---
+++ test/Double/Reporter/NullReporter.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2021 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/phpunit-slow-test-detector
+ */
+
+namespace Ergebnis\PHPUnit\SlowTestDetector\Test\Double\Reporter;
+
+use Ergebnis\PHPUnit\SlowTestDetector\Reporter;
+use Ergebnis\PHPUnit\SlowTestDetector\SlowTest;
+
+final class NullReporter implements Reporter\Reporter
+{
+    public function report(SlowTest ...$slowTests): string
+    {
+        return '';
+    }
+}
