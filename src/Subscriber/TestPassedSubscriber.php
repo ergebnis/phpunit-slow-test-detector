0a16ced6ab9627e6352dff0d4831e1e7caef7160
Andreas Möller <am@localheinz.com>
2021-01-26 08:56:53 +0100

    Enhancement: Allow configuring maximum duration with slowThreshold annotation

--- src/Subscriber/TestPassedSubscriber.php
+++ src/Subscriber/TestPassedSubscriber.php
@@ -18,6 +18,7 @@ use Ergebnis\PHPUnit\SlowTestDetector\MaximumDuration;
 use Ergebnis\PHPUnit\SlowTestDetector\SlowTest;
 use Ergebnis\PHPUnit\SlowTestDetector\TimeKeeper;
 use PHPUnit\Event;
+use PHPUnit\Util;
 
 final class TestPassedSubscriber implements Event\Test\PassedSubscriber
 {
@@ -44,16 +45,50 @@ final class TestPassedSubscriber implements Event\Test\PassedSubscriber
             $event->telemetryInfo()->time()
         );
 
-        if (!$duration->isGreaterThan($this->maximumDuration->toTelemetryDuration())) {
+        $maximumDuration = $this->resolveMaximumDuration($event->test());
+
+        if (!$duration->isGreaterThan($maximumDuration)) {
             return;
         }
 
         $slowTest = SlowTest::fromTestDurationAndMaximumDuration(
             $event->test(),
             $duration,
-            $this->maximumDuration->toTelemetryDuration()
+            $maximumDuration
         );
 
         $this->collector->collect($slowTest);
+    }
+
+    private function resolveMaximumDuration(Event\Code\Test $test): Event\Telemetry\Duration
+    {
+        $annotations = Util\Test::parseTestMethodAnnotations(
+            $test->className(),
+            $test->methodName()
+        );
+
+        if (!\array_key_exists('method', $annotations)) {
+            return $this->maximumDuration->toTelemetryDuration();
+        }
+
+        if (!\is_array($annotations['method'])) {
+            return $this->maximumDuration->toTelemetryDuration();
+        }
+
+        if (!\array_key_exists('slowThreshold', $annotations['method'])) {
+            return $this->maximumDuration->toTelemetryDuration();
+        }
+
+        if (!\is_array($annotations['method']['slowThreshold'])) {
+            return $this->maximumDuration->toTelemetryDuration();
+        }
+
+        $slowThreshold = \reset($annotations['method']['slowThreshold']);
+
+        if (1 !== \preg_match('/^\d+$/', $slowThreshold)) {
+            return $this->maximumDuration->toTelemetryDuration();
+        }
+
+        return MaximumDuration::fromMilliseconds((int) $slowThreshold)->toTelemetryDuration();
     }
 }
